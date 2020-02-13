86c6321b72aa382fa5d132ef77d245916a5bc5f1
Coop <coop.coding@gmail.com>
2020-02-13 13:02:38 +1100

    Update

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,9 +1,10 @@
-// @ts-ignore
+// @flow
+// @flow-ignore-line
 if (module.hot) module.hot.accept()
-import React from "react"
-import ReactDOM from "react-dom"
-import "../css/index.css"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '../css/index.css'
 
+const mountNode = document.getElementById('app')
 
-var mountNode = document.getElementById("app")
-ReactDOM.render(<h1>Hello</h1>, mountNode)
+mountNode && ReactDOM.render(<h1>Hello</h1>, mountNode)
